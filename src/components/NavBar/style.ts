aafad514f08cac2c33ec09961636ae3f95fce19c
Derick-Almeida <d.a.s2000silva14@gmail.com>
2023-06-03 19:04:18 -0300

    feat: NavBar

---
+++ src/components/NavBar/style.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ul = styled.ul`
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+`;
+
+export const li = styled.li``;
