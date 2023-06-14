e8a44b1dcc7c32b7bcdd67b361ee81fe4f5d70af
Derick-Almeida <d.a.s2000silva14@gmail.com>
2023-06-14 20:39:15 -0300

    feat: cart card control

--- src/components/CartCard/style.ts
+++ src/components/CartCard/style.ts
@@ -10,20 +10,14 @@ export const card = styled.li`
 
   padding: 1%;
   border-bottom: 1px solid var(--black-2);
-  position: relative;
 
   transition: background 0.3s, filter 0.3s;
-
-  &:hover {
-    cursor: pointer;
-    background: var(--black-2);
-    filter: brightness(0.8);
-  }
 `;
 
 export const content = styled.div`
   width: 100%;
   margin-left: 10px;
+  position: relative;
 `;
 
 export const img = styled.img`
@@ -55,4 +49,75 @@ export const text = styled.p`
   font-size: 0.9rem;
 `;
 
-export const controls = styled.div``;
+export const controlsContainer = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+
+  min-width: 50%;
+  max-width: 50%;
+  min-height: 5vh;
+  max-height: 5vh;
+
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+
+  background: var(--grey-1);
+  border-radius: 0.5rem;
+`;
+
+export const controls = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+
+  min-width: 100%;
+`;
+
+export const button = styled.button`
+  border: none;
+  background: none;
+
+  font-size: 1.5rem;
+  font-weight: 500;
+  color: var(--grey-3);
+
+  padding: 0 10%;
+  border-radius: 50%;
+
+  transition: color 0.4s, text-shadow 0.4s;
+
+  &:hover {
+    color: var(--green-1);
+    text-shadow: 0.05rem 0.05rem 0.1rem var(--green-1);
+  }
+
+  &:nth-child(1):hover {
+    color: var(--red);
+    text-shadow: 0.05rem 0.05rem 0.1rem var(--red);
+  }
+`;
+
+export const exclude = styled.button`
+  border: none;
+  background: none;
+  margin-top: -1vh;
+
+  font-size: 1rem;
+  font-weight: 400;
+  color: var(--black-1);
+
+  transition: color 0.4s, text-shadow 0.4s;
+
+  &:hover {
+    color: var(--red);
+    text-shadow: 0.05rem 0.05rem 0.1rem var(--red);
+  }
+`;
+
+export const span = styled.span`
+  color: var(--grey-2);
+  font-size: 1.2rem;
+  font-weight: bold;
+`;
